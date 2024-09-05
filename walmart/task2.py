ed704ce8344c2f0d0f2237ea42e8549f55a0c50e
yadavanujkumar <thunderstruck9999@gmail.com>
2024-09-05 22:54:42 +0530

    survey analysis

--- walmart/task2.py
+++ walmart/task2.py
@@ -12,46 +12,57 @@ sales_branch2 = "SELECT * FROM df WHERE Branch='B'"
 sales_result2 = psql.sqldf(sales_branch2, locals())
 print(sales_result2)
 
+sales_branch3 = "SELECT * FROM df WHERE Branch='C'"
+sales_result3 = psql.sqldf(sales_branch3, locals())
+print(sales_result3)
 
+print("-----------------------------------------------------------------------------------------")
 # 2.Find the total sales for each product line.
 productline = "SELECT Product_line ,SUM(Total) as Total FROM df group by product_line"
 productline_result = psql.sqldf(productline, locals())
 print(productline_result)
+print("-----------------------------------------------------------------------------------------")
 
 
 # 3.List all sales transactions where the payment method was 'Cash'.
 sales_cash = "select * from df where payment =='Cash'"
 result_sales_cash = psql.sqldf(sales_cash,locals())
 print(result_sales_cash)
+print("-----------------------------------------------------------------------------------------")
 
 
 # 4.Calculate the total gross income generated in each city. 
 gross_income = "select city, sum(gross_income) as Total_Gross_Income from df group by city"
 result_gross_income = psql.sqldf(gross_income, locals())
 print(result_gross_income)
+print("-----------------------------------------------------------------------------------------")
 
 
 # 5.Find the average rating given by customers in each branch. 
 avg_rating = "select branch, customer_type, avg(rating) as avg_rating from df group by branch, customer_type"
 result_avg_rating = psql.sqldf(avg_rating, locals())
 print(result_avg_rating)
+print("-----------------------------------------------------------------------------------------")
 
 
 # 6.Determine the total quantity of each product line sold.
 product_quantity = "select product_line, sum(Quantity) as Total_Quantity from df group by product_line"
 result_product_quantity = psql.sqldf(product_quantity, locals())
 print(result_product_quantity)
+print("-----------------------------------------------------------------------------------------")
 
 
 # 7.List the top 5 products by unit price. 
 unitprice = "select * from df order by unit_price DESC LIMIT 5"
 result_unitprice = psql.sqldf(unitprice, locals())
 print(result_unitprice)
+print("-----------------------------------------------------------------------------------------")
 
 # 8.Find sales transactions with a gross income greater than 30.
 sales_trans = "select payment, gross_income from df where gross_income > 30 order by gross_income"
 result_sales_trans = psql.sqldf(sales_trans, locals())
 print(result_sales_trans)
+print("-----------------------------------------------------------------------------------------")
 
 
 # 9.Retrieve sales transactions that occurred on weekends.
@@ -59,24 +70,28 @@ df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y')
 weekend = "select * from df where strftime('%w', Date) IN ('5', '6')"
 result_weekend = psql.sqldf(weekend, locals())
 print(result_weekend)
+print("-----------------------------------------------------------------------------------------")
 
 # 10.Calculate the total sales and gross income for each month.
 df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y')
 month = "select sum(total) as Total, sum(gross_income) as Gross_Income, strftime('%m', Date) as Month from df group by Month"
 result_month = psql.sqldf(month, locals())
 print(result_month)
- 
+print("-----------------------------------------------------------------------------------------")
+
  
 # 11.Find the number of sales transactions that occurred after 6 PM.
 sales_after6 = "select count(*) as total_transaction from df where time > '18:00:00'"
 result_sales_after6 = psql.sqldf(sales_after6, locals())
 print(result_sales_after6)
+print("-----------------------------------------------------------------------------------------")
 
 
 # 12.List the sales transactions that have a higher total than the average total of all transactions. 
 higher = "select * from df where total > (SELECT avg(Total) FROM df)"
 result_higher = psql.sqldf(higher, locals())
 print(result_higher)
+print("-----------------------------------------------------------------------------------------")
 
 
 # 13. Calculate the cumulative gross income for each branch by date.
@@ -84,9 +99,11 @@ cumulative = "select sum(gross_income) as gross_income, branch, date from df gro
 result_cumulative = psql.sqldf(cumulative, locals())
 print(result_cumulative)
 
+print("-----------------------------------------------------------------------------------------")
 
 # 15. Find the total cogs for each customer type in each city.
 total_cog = "select sum(cogs) as total_cogs, customer_type, city from df group by customer_type, city "
 result_total_cog = psql.sqldf(total_cog, locals())
 print(result_total_cog)
+print("-----------------------------------------------------------------------------------------")
 
